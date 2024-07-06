1c9722661c48c7e86ec6b2163ddfbbf834f13c85
karem ois <karem@karems-MacBook-Pro.local>
2024-07-06 19:27:48 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Hello World Kotlin")
+    println("What is The different between Java and Kotlin")
+    println("What is The different between Java and Kotlin")
+}
